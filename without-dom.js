6e57d12d1f644e82c8daa1f827e0210224c5409e
Eric Pradana <ericprd_@outlook.com>
2022-09-25 19:26:40 +0700

    use loop to play game

--- without-dom.js
+++ without-dom.js
@@ -29,10 +29,33 @@ changeTileColor(2, 1);
 
 console.log();
 
-if (selectedTilesIndex.length >= 9) {
-  const interval = setInterval(() => {
-    revertTileColor(selectedTilesIndex[0][0], selectedTilesIndex[0][1]);
-    selectedTilesIndex.shift();
+let play = confirm("Let's start the game");
+while (play) {
+  let row;
+  let column;
+  if (selectedTilesIndex.length < 9) {
+    row = Number(prompt("Input Row"));
+    column = Number(prompt("Input column"));
+  }
+
+  if (selectedTilesIndex.length > 8) {
+    const interval = setInterval(() => {
+      revertTileColor(selectedTilesIndex[0][0], selectedTilesIndex[0][1]);
+      selectedTilesIndex.shift();
+      const next = confirm("Continue?");
+      if (!next) return;
+    }, 500);
     if (selectedTilesIndex.length === 0) clearInterval(interval);
-  }, 500);
+    const playAgain = confirm("Continue?");
+    if (!playAgain) play = false;
+  }
+  if (row == "null" || column == "null") break;
+  if (isNaN(row) || isNaN(column)) {
+    alert("Input is not a number");
+    play = false;
+    break;
+  }
+  changeTileColor(row, column);
+  console.log(tiles);
+  play = confirm("Continue?");
 }
